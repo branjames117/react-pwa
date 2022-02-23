2f70498d54e94cca2a322190c8881effd6b768c5
Brandon Hoskins <branjames117@gmail.com>
2022-02-22 22:08:16 -0600

    registering service worker

--- src/App.js
+++ src/App.js
@@ -1,12 +1,29 @@
-import logo from './logo.svg';
+import { useState, useEffect } from 'react';
 import './App.css';
 
 function App() {
+  const [data, setData] = useState([]);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      const result = await fetch('https://orangevalleycaa.org/api/videos');
+      const response = await result.json();
+      setData(response);
+    };
+    fetchData();
+  }, []);
+
   return (
     <div className='App'>
-      <header>
-        <h1>Videos</h1>
+      <header className='App-header'>
+        <h1>Art Videos</h1>
       </header>
+      {data.map((video) => (
+        <div key={video.id}>
+          <h2>{video.name}</h2>
+          <video height={200} controls src={video.video_url} />
+        </div>
+      ))}
     </div>
   );
 }
