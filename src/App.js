76aa429dbb9be008d4835497b734f49ded85c920
Brandon Hoskins <branjames117@gmail.com>
2022-02-22 22:33:33 -0600

    preparing deploy to netlify

--- src/App.js
+++ src/App.js
@@ -1,29 +1,33 @@
-import { useState, useEffect } from 'react';
+// import { useState, useEffect } from 'react';
 import './App.css';
+import video from './videos/sculpture.mp4';
 
 function App() {
-  const [data, setData] = useState([]);
+  // const [data, setData] = useState([]);
 
-  useEffect(() => {
-    const fetchData = async () => {
-      const result = await fetch('https://orangevalleycaa.org/api/videos');
-      const response = await result.json();
-      setData(response);
-    };
-    fetchData();
-  }, []);
+  // useEffect(() => {
+  //   const fetchData = async () => {
+  //     const result = await fetch('https://orangevalleycaa.org/api/videos');
+  //     const response = await result.json();
+  //     setData(response);
+  //   };
+  //   fetchData();
+  // }, []);
 
   return (
     <div className='App'>
       <header className='App-header'>
         <h1>Art Videos</h1>
       </header>
-      {data.map((video) => (
+      <div>
+        <video src={video} controls height={200} />
+      </div>
+      {/* {data.map((video) => (
         <div key={video.id}>
           <h2>{video.name}</h2>
           <video height={200} controls src={video.video_url} />
         </div>
-      ))}
+      ))} */}
     </div>
   );
 }
